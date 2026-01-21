82e49f4e392ef0cd9a9b993729252422776ec624
Zach Way <zachway1996@gmail.com>
2026-01-21 14:29:01 -0500

    Added filter profiles

--- utils.py
+++ utils.py
@@ -1,1 +1,40 @@
 from astropy.tables import Table
+from glob import glob
+import numpy as np
+import os 
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+filter_files = glob(f"{dir_path}/filters/*")
+surveys = [ff.split("_")[1].split(".")[0] for ff in filter_files]
+filters = [ff.split(".")[1] for ff in filter_files]
+
+
+class phot_filter:
+    def __init__(self, survey, filter):
+        self.survey = survey
+        self.filter = filter
+
+        self.surveys = surveys
+        self.filters = filters
+
+        if self.survey not in self.surveys or self.filter not in self.filters:
+            raise Exception(
+                f"Survey {self.survey} and filter {self.filter} not in the "
+                f"valid surveys ({self.surveys}) and filters ({self.filters})"
+            )
+
+        survey_dict = {"2MASS": "2MASS_2MASS",
+                       "GAIA3": "GAIA_GAIA3",
+                       "WISE": "WISE_WISE",
+                       }
+        filter_filename = f"{survey_dict[self.survey]}.{self.filter}.dat"
+        try:
+            self.data = np.genfromtxt(filter_filename, delimiter=" ")
+        except Exception as e:
+            print(f"Something wrong with filename {filter_filename}")
+            raise e
+
+        self.wvl = self.data.T[0]
+        self.trans = self.data.T[1]
+    
